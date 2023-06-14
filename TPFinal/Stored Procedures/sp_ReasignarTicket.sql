ebd266ca02274e337b75e01f84ef337c2af0b161
sosamatias1 <sosamatias171@gmail.com>
2023-06-14 09:59:08 -0300

    se agrega la transaccion para realizar el rollback tran si da error, en el sp reasignar ticket

--- TPFinal/Stored Procedures/sp_ReasignarTicket.sql
+++ TPFinal/Stored Procedures/sp_ReasignarTicket.sql
@@ -12,27 +12,28 @@ CREATE PROCEDURE dbo.sp_ReasignarTicket(
 
 AS
 	BEGIN TRY
-	IF EXISTS(SELECT * FROM ticket WHERE id_ticket = @id_ticket AND @duenio_id = empleado_id)
-	BEGIN
-			IF EXISTS (SELECT * FROM empleado WHERE id_empleado = @empleado_id and estado_empleado_id = 1)
+		BEGIN TRAN
+			IF EXISTS(SELECT * FROM ticket WHERE id_ticket = @id_ticket AND @duenio_id = empleado_id)
 			BEGIN
-				UPDATE ticket
-				SET empleado_id = @empleado_id
-				WHERE id_ticket = @id_ticket
+					IF EXISTS (SELECT * FROM empleado WHERE id_empleado = @empleado_id and estado_empleado_id = 1)
+					BEGIN
+						UPDATE ticket
+						SET empleado_id = @empleado_id
+						WHERE id_ticket = @id_ticket
+					END
+					ELSE
+					BEGIN
+						RAISERROR('El empleado seleccionado esta inactivo', 16, 1)
+					END
 			END
 			ELSE
 			BEGIN
-				RAISERROR('El empleado seleccionado esta inactivo', 16, 1)
+				RAISERROR('No se pudo reasignar el ticket porque el ticket seleccionado no existe o no es el dueño del ticket', 16, 1)
 			END
-	END
-	ELSE
-	BEGIN
-		RAISERROR('No se pudo reasignar el ticket porque el ticket seleccionado no existe o no es el dueño del ticket', 16, 1)
-	END
+		COMMIT TRAN
 	END TRY
 	BEGIN CATCH
-		SELECT 
-			ERROR_NUMBER() AS Numero_Error,
-			ERROR_MESSAGE() AS Mensaje_Error 
+		ROLLBACK TRAN
+		THROW
 	END CATCH
 	
